60ed59757737bbd44c3e6bfcd4cf9efe942eef02
Kim Joon Kyoung <gang_3@naver.com>
2024-05-28 18:01:44 +0900

    컴포넌트 이용한 vue 연습

---
+++ chapter07/checkbox-item3/src/components/CheckboxItem2.vue
@@ -0,0 +1,12 @@
+<template>
+  <li><input type="checkbox" :checked="idol.checked" />{{ idol.name }}</li>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxItem2',
+  // props : 부모 컴포넌트한테 받아온 속성
+  // 받아올 속성이 많은 경우 객체를 통째로 넘겨준다
+  props: ['idol'],
+};
+</script>
