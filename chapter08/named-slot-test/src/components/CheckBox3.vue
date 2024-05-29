3df96d48a45e062461bef12aaa9cb29f84a50573
Kim Joon Kyoung <gang_3@naver.com>
2024-05-29 17:52:09 +0900

    slot 영역 여러 개 지정 시 각 영역에 이름을 배정해 사용

---
+++ chapter08/named-slot-test/src/components/CheckBox3.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- icon 이라는 이름의 슬롯을 받아옴 -->
+    <slot name="icon"></slot>
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="$emit('check-changed', { id, checked: $event.target.checked })"
+    />
+    <!-- label 이라는 이름의 슬롯을 받아옴 -->
+    <slot name="label">Item</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckBox3',
+  props: ['id', 'checked'],
+};
+</script>
