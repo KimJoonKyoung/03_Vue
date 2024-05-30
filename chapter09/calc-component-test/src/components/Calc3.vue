20ddfc5c4828d777a2eefbc46697fbc7816faf0a
Kim Joon Kyoung <gang_3@naver.com>
2024-05-30 14:57:54 +0900

    reactive( ) 참조형(객체 등)에 대한 반응성을 가지도록 함

---
+++ chapter09/calc-component-test/src/components/Calc3.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="state.x" /> <br />
+    Y : <input type="text" v-model.number="state.y" /> <br />
+    <button @click="calcAdd">계산</button><br />
+    <div>결과 : {{ state.result }}</div>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+  name: 'Calc3',
+  setup() {
+    // computed로 계산해주기 때문에 result 초기화 필요 X
+    const state = reactive({ x: 10, y: 20, result: 30 });
+    // 기존 옵션 API의 computed 옵션과 동일한 역할
+    const calcAdd = () => {
+      state.result = state.x + state.y;
+    };
+    return { state, calcAdd };
+  },
+};
+</script>
