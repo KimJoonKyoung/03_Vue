5fc2ef269df3e7d7b7cb25524af4a6d7d993db29
Kim Joon Kyoung <gang_3@naver.com>
2024-05-30 17:55:50 +0900

    사용자 정의 v-model 만들기

---
+++ chapter08/v-model-test/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- 자식의 name을 searchName으로 전달하고 다시 자식의 변경값을 받아옴 -->
+    <!-- <InputName :name="searchName" @update:name="changeSearchName" /> 아래 코드로 생략 가능-->
+    <InputName v-model:name="searchName" />
+    <h3>검색어 : {{ searchName }}</h3>
+  </div>
+</template>
+
+<script>
+import InputName from './components/InputName.vue';
+
+export default {
+  name: 'App',
+  components: { InputName },
+  data() {
+    return { searchName: 'John' };
+  },
+};
+</script>
