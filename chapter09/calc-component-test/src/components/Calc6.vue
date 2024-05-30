b6c5284b0ca30ecde74c75f007ed620baec39066
Kim Joon Kyoung <gang_3@naver.com>
2024-05-30 14:58:38 +0900

    reactive 사용해 객체 형태로 상태 관리 watch 통해 객체의 특정 속성 변화 감시

---
+++ chapter09/calc-component-test/src/components/Calc6.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="state.x" /> <br />
+
+    <div>결과 : {{ state.result }}</div>
+  </div>
+</template>
+
+<script>
+import { reactive, watch } from 'vue';
+
+export default {
+  name: 'Calc6',
+  setup() {
+    const state = reactive({ x: 0, result: 0 });
+    // 첫 번째 인자로는 반응성 데이터가 와야하기 때문에 state.x 불가능
+    // 화살표 함수로 객체 내의 데이터를 접근해야 한다
+    watch(
+      () => state.x,
+      (current, old) => {
+        console.log(`${old} -> ${current}`);
+        state.result = current * 2;
+      }
+    );
+    return { state };
+  },
+};
+</script>
