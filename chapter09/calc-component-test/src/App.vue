e69b9bd5ee5a436f4f6072868a65139d1a56c5fc
Kim Joon Kyoung <gang_3@naver.com>
2024-05-30 14:57:04 +0900

    setup( ) 메서드 반응성 가진 상태 데이터, 계산된 속성, 메서드, 생명주기 훅을 작성 객체 형태로 리턴 -> 작성한 데이터나 메서드를 템플릿에서 이용

---
+++ chapter09/calc-component-test/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Calc />
+  </div>
+</template>
+
+<script>
+import Calc from './components/Calc8.vue';
+
+export default {
+  name: 'App',
+  components: { Calc },
+};
+</script>
