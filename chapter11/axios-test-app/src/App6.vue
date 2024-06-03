6306626f4a157f71ac25df3b1e4b930cf6b35631
Kim Joon Kyoung <gang_3@naver.com>
2024-06-03 17:43:46 +0900

    async-await 사용한 오류 처리 try-catch 내에서 요청 수행 오류 발생 시 catch 블럭에서 처리

---
+++ chapter11/axios-test-app/src/App6.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>콘솔을 확인합니다.</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+const requestAPI = async () => {
+  const url = '/api/todos2';
+  // API 요청 성공 시
+  try {
+    const response = await axios.get(url, { timeout: 900 });
+    console.log('# 응답객체 : ', response);
+  } catch (e) {
+    // API 요청 실패 시
+    console.log('## 다음 오류가 발생했습니다.');
+    // instanceof : e가 Error 타입인지 체크
+    if (e instanceof Error) console.log(e.message);
+    else console.log(e);
+  }
+};
+
+requestAPI();
+</script>
