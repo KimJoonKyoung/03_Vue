f07590314d39ed6366c6a607b140e2f1627b6511
Kim Joon Kyoung <gang_3@naver.com>
2024-06-03 17:30:31 +0900

    axios.post() 메서드를 이용한 POST 요청 처리

---
+++ chapter11/axios-test-app/src/App5.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>콘솔을 확인합니다.</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+const requestAPI = async () => {
+  const url = '/api/todos';
+
+  // post 메서드의 body 부분에 해당
+  let data = { todo: '윗몸일으키기 3세트', desc: '너무 빠르지 않게...' };
+
+  // 해당 url에 data를 담아서 날림
+  const resp1 = await axios.post(url, data);
+  console.log(resp1.data);
+};
+
+requestAPI();
+</script>
