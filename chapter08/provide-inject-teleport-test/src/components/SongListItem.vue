019b2e96fb26972c6a42f2941c7c82a4cc7c8ea7
Kim Joon Kyoung <gang_3@naver.com>
2024-06-04 17:49:36 +0900

    -

--- chapter08/provide-inject-teleport-test/src/components/SongListItem.vue
+++ chapter08/provide-inject-teleport-test/src/components/SongListItem.vue
@@ -1,5 +1,7 @@
 <template>
   <li>
+    <!-- song에 done이 true면 icons의 checked 내에 있는 값으로 클래스 지정 -->
+    <!-- song에 done이 false면 icons의 unchecked 내에 있는 값으로 클래스 지정 -->
     <i :class="song.done ? icons.checked : icons.unchecked"></i>
     {{ song.title }}
   </li>
@@ -8,6 +10,7 @@
 <script>
 export default {
   name: 'SongListItem',
+  // 부모에서 provide한 항목을 inject로 가져올 수 있다.
   inject: ['icons'],
   props: ['song'],
 };
